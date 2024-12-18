fcaf3991b2a2fe62ab391998e568fe61d1c48a2e
Danny Lynch <lynch4360@gmail.com>
2024-12-18 20:05:07 +0000

    Add Logic and XAML from MainPage to Game Page

--- Views/StartPage.xaml.cs
+++ Views/StartPage.xaml.cs
@@ -2,18 +2,19 @@ namespace CrossPlatformProject2.Views;
 
 public partial class StartPage : ContentPage
 {
-	public StartPage()
-	{
-		InitializeComponent();
-	}
-
-	private void btnSettings_Clicked(object sender, EventArgs e)
+    public StartPage()
     {
-		Shell.Current.GoToAsync(nameof(SettingsPage));
+        InitializeComponent();
     }
 
-    private void btnStartTrivia_Clicked(object sender, EventArgs e)
+    private void btnSettings_Clicked(object sender, EventArgs e)
     {
-		Shell.Current.GoToAsync(nameof(GamePage));
+        Shell.Current.GoToAsync(nameof(SettingsPage));
+    }
+
+    private async void btnStartTrivia_Clicked(object sender, EventArgs e)
+    {
+
+        Shell.Current.GoToAsync(nameof(GamePage));
     }
 }
