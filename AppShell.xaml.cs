216c28f4d0c76e9abb1c77b9ddb1c44aacd08b58
Danny Lynch <lynch4360@gmail.com>
2025-01-06 22:51:36 +0000

    Simplify AppShell.cs by removing route registrations

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,17 +1,10 @@
-﻿using CrossPlatformProject2.Views;
-
-namespace CrossPlatformProject2
+﻿namespace CrossPlatformProject2
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
-
-
-            Routing.RegisterRoute(nameof(StartPage), typeof(StartPage));
-            Routing.RegisterRoute(nameof(GamePage), typeof(GamePage));
-            Routing.RegisterRoute(nameof(SettingsPage), typeof(SettingsPage));
         }
     }
 }
