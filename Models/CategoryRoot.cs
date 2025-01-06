d88af5a8270c90e3f40779ab6b349779170ec7fa
Danny Lynch <lynch4360@gmail.com>
2025-01-06 23:52:07 +0000

    Add GameState and CategoryRoot models for game state management and trivia category handling

---
+++ Models/CategoryRoot.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace CrossPlatformProject2.Models
+{
+    public class CategoryRoot
+    {
+        [JsonProperty("trivia_categories")]
+        public List<TriviaCategory>? TriviaCategories { get; set; } = new List<TriviaCategory>();
+    }
+}
+
---
+++ Models/GameState.cs
@@ -0,0 +1,20 @@
+﻿namespace CrossPlatformProject2.Models
+{
+    public class GameState //game state page to save
+    {
+        public Dictionary<string, int> PlayerScores { get; set; }
+        public int CurrentQuestionIndex { get; set; }
+        // public List<QuestionModel> RemainingQuestions { get; set; }
+        public int CurrentPlayerIndex { get; set; }
+        public int TotalQuestions { get; set; }
+        public string SelectedDifficulty { get; set; }
+        public string SelectedCategory { get; set; }
+
+        public int selectedCategoryId { get; set; }//category id
+
+        public string SelectedPlayers { get; set; }//added to store selected players
+        public List<string> PlayerNames { get; set; }
+
+        public List<QuestionModel> TriviaQuestions { get; set; } //added to store question 
+    }
+}
