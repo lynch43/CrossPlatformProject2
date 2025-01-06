91ef5230ebeb2f06ea1a565557d0faed8a1c0f76
Danny Lynch <lynch4360@gmail.com>
2025-01-06 22:40:01 +0000

    Enhance App.xaml.cs: Add global PlayerScores dictionary and wrap MainPage in NavigationPage

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,15 @@
-﻿using CrossPlatformProject2.Views;
-
-namespace CrossPlatformProject2
+﻿namespace CrossPlatformProject2
 {
     public partial class App : Application
     {
+        public Dictionary<string, int> PlayerScores { get; set; } = new Dictionary<string, int>();//player scores accessible globaly
         public App()
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new StartPage());
+
+            MainPage = new NavigationPage(new MainPage()); //wrap MainPage in NavigationPage
         }
     }
 }
+
