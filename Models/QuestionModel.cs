38fed95f2ff16cfc5c5c3d771b4da391bafae263
Danny Lynch <lynch4360@gmail.com>
2025-01-06 23:54:49 +0000

    Add Result and QuestionModel classes for trivia question representation and management

---
+++ Models/QuestionModel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CrossPlatformProject2.Models
+{
+    public class QuestionModel //model to represent each trivia question 
+    {
+        public string Question { get; set; }//the question
+        public string CorrectAnswer { get; set; }//correct answer
+        //incorrect answer lsit
+        public List<string> IncorrectAnswers { get; set; }
+    }
+}
---
+++ Models/Result.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace CrossPlatformProject2.Models
+{
+    public class Result
+    {
+        public string type { get; set; } //question type
+        public string difficulty { get; set; }//difficulty level
+        public string category { get; set; }//question category
+        public string question { get; set; }//the question
+        public string correct_answer { get; set; }//the answer
+        public List<string> incorrect_answers { get; set; }//incorrect answers
+
+    }
+}
