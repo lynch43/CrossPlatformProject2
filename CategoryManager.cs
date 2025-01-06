bcd0df06e1371548aebc6861e4ed65fef2ccb92c
Danny Lynch <lynch4360@gmail.com>
2025-01-06 23:10:03 +0000

    Add CategoryManager class for managing trivia categories with centralized dictionary of names and IDs

---
+++ CategoryManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossPlatformProject2
+{
+    public static class CategoryManager
+    {
+        // Define the Categories dictionary
+        public static readonly Dictionary<string, int> Categories = new Dictionary<string, int>
+    {
+        { "General Knowledge", 9 },
+        { "Science and Nature", 17 },
+        { "Entertainment: Video Games", 15 },
+        { "Entertainment: Film", 11 },
+        { "Music", 12 },
+        { "Books", 10 },
+        { "Art", 25 }
+    };
+    }
+}
